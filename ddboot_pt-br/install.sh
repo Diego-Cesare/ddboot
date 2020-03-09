c2d29a795d98953a2b3ba6e7eefa8fd3e8642065
diego <diegocesare300491@gmail.com>
2020-03-09 18:06:33 -0300

    Mudanças no caminho de instalção e de execução

--- ddboot_pt-br/install.sh
+++ ddboot_pt-br/install.sh
@@ -3,14 +3,14 @@
 echo "Tornando o arquivo ddboot executavel"
 sleep 3
 sudo chmod +x ddboot
-sleep 3 
+sleep 3
 echo "Pronto!"
 sleep 2
 clear
 
 echo "Copiando o arquivo ddboot para / bin"
 sleep 3
-sudo cp ddboot /bin
+sudo cp ddboot /usr/bin
 sleep 2
 echo "Pronto!"
 
